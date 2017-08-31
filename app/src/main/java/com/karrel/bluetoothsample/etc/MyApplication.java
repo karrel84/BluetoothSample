917952c78188db1011ce3b8bf6072db999c0aa9d
karrel <karrel84@naver.com>
2017-08-31 15:43:41 +0900

    메세지 송신시 스크롤 최상단으로 이동

--- app/src/main/java/com/karrel/bluetoothsample/etc/MyApplication.java
+++ app/src/main/java/com/karrel/bluetoothsample/etc/MyApplication.java
@@ -17,7 +17,7 @@ public class MyApplication extends Application {
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
 
-        RLog.setEnable(true);
+        RLog.setEnable(false);
         Realm.init(this);
     }
 }
